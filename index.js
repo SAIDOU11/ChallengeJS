a45122edfda2b97a922e5421cbc78f57a68361fd
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-08-31 05:54:28 +0200

    array

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-import placeholderPropertyObj from './properties/placeholderPropertyObj.js';
 import propertyForSaleArr from './properties/propertyForSaleArr.js';
+import placeholderPropertyObj from './properties/placeholderPropertyObj.js';
 
-const getPropertyHtml = () => {
-  propertyForSaleArr
+const getPropertyHtml = (propertiesArr = [placeholderPropertyObj]) => {
+  return propertiesArr
     .map((property) => {
-      const { comment, image, priceGBP, roomsM2, propertyLocation } = property;
-      roomsM2.reduce();
-      return `
+      console.log(property);
+      const { comment, image, priceGBP, propertyLocation, roomsM2 } = property;
+      const roomSurface = roomsM2.reduce(
+        (total, currentSurface) => total + currentSurface
+      );
+      return ` 
     <section class="card">
     <img src="/images/${image}">
     <div class="card-right">
-        <h2>${propertyLocation} </h2>
-        <h3>${priceGBP} </h3>
-        <p>${comment} </p>
-        <h3>TOTAL SIZE IN SQUARE METRES m&sup2;</h3>
+        <h2>${propertyLocation}</h2> 
+        <h3>${priceGBP}</h3>
+        <p>${comment}</p>
+        <h3>${roomSurface} m&sup2;</h3>
     </div>
 </section> 
     `;
     })
     .join('');
 };
-const container = document.createElement('div');
-container.append(getPropertyHtml());
 
-document.getElementById('container').innerHTML = container;
+document.getElementById('container').innerHTML =
+  getPropertyHtml(propertyForSaleArr);
+
+document.getElementById('container').innerHTML = getPropertyHtml();
